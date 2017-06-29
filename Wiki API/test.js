70869b4a97d7056fed3ea43a89db59180dd7a53a
bunnycat <bunnycat@ohai.pc>
2017-06-30 03:12:12 +0400

    Add fcc projects

---
+++ Wiki API/test.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  $('#main').on('submit', function(event){
+    event.preventDefault();
+
+    var query = $('input').val();
+    console.log(query);
+
+    var ajax = {
+      url: "https://en.wikipedia.org/w/api.php",
+      data: {
+        action: 'opensearch',
+        format: 'json',
+        formatversion: '2',
+        search: query,
+        namespace: '0'
+      },
+      dataType: 'jsonp',
+      success: function(data){
+        console.log(data);
+        if ($('#results').hasClass('added')){
+          $('#results').empty().removeClass('added');
+        }
+        var card ='';
+        if (data[1].length === 0){
+          card = "<h3>No results for '"+query+"'</h3>";
+          $('#results').last().append(card).addClass('added');
+        } else {
+            for (var i= 0; i<data[1].length; i++){
+            var title = data[1][i];
+            var snippet = data[2][i];
+            var link = data[3][i];
+
+            card = "<div data-link='" +link+ "'><h1>"+ title +"</h1><h3>"+snippet+"</h3></div>";
+            $('#results').last().append(card).addClass('added');
+          }
+        }
+      }
+    };
+
+    $.ajax(ajax);
+  });
+
+  $('#results').on('click', 'div', function(event){
+    console.log($(this).attr('data-link'));
+    window.open($(this).attr('data-link'));
+  });
+
+});
